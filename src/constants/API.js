b98a45965d56de9d87681275ed036e7e1f03cd03
Tran Minh Loc <91857852+minhlocdev@users.noreply.github.com>
2024-03-21 22:00:30 +0700

    Update API.js + SSL set up

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://35.186.148.127:32813/api/fams";
+export const BASE_URL = "https://fams.asia/api/fams";
 export const BASE_URL_1 = "https://65d8432ec96fbb24c1bb11b2.mockapi.io"
 export const BASE_URL_2 = "https://653d1d13f52310ee6a99e3b7.mockapi.io/class"
